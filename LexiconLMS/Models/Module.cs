245ea25d3c2e94729150ed3dfee5fe38e6dc86dc
Lena Widegren <widenzia@gmail.com>
2017-03-22 11:56:07 +0100

    Change Modules Controller and Index view

--- LexiconLMS/Models/Module.cs
+++ LexiconLMS/Models/Module.cs
@@ -6,19 +6,85 @@ using System.Web;
 
 namespace LexiconLMS.Models
 {
-    public class Module 
+    public class Module
     {
         public int Id { get; set; }
 
         [Required(AllowEmptyStrings = false)]
         public string Name { get; set; }
-    
+
         public string Description { get; set; }
         public int CourseId { get; set; }
 
         // Navigation properties
-        //public virtual Course Course { get; set; }
+        public virtual Course Course { get; set; }
         public virtual ICollection<Activity> Activities { get; set; }
         //public virtual ICollection<Document> Documents { get; set; }
+    }
+
+    public class ModuleSingleViewModel
+    {
+        public ModuleSingleViewModel()
+        {
+
+        }
+
+        public ModuleSingleViewModel(Module module)
+        {
+            CourseName = module.Course.Name;
+            CourseId = module.CourseId;
+            Name = module.Name;
+            Description = module.Description;
+        }
+
+        public ModuleSingleViewModel(Course course)
+        {
+            CourseName = course.Name;
+            CourseId = course.Id;
+        }
+
+        public string CourseName { get; set; }
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class ModuleIndexViewModel
+    {
+        public ModuleIndexViewModel(Course course, IEnumerable<ModuleViewModel> modules)
+        {
+            CourseName = course.Name;
+            CourseId = course.Id;
+            Modules = modules;
+        }
+
+        public string CourseName { get; }
+        public int CourseId { get; }
+        public IEnumerable<ModuleViewModel> Modules { get; }
+
+    }
+
+    public class ModuleViewModel
+    {
+        public ModuleViewModel(Module module)
+        {
+            Id = module.Id;
+            Name = module.Name;
+            Description = module.Description;
+
+            ActivityCount = module.Activities.Count;
+
+            var sortedActivities = module.Activities.OrderBy(a => a.StartDate);
+            StartDate = sortedActivities.FirstOrDefault()?.StartDate;
+            EndDate = sortedActivities.LastOrDefault()?.EndDate;
+        }
+
+        public int Id { get; }
+        public int ActivityCount { get; set; }
+        public string Name { get; }
+        public string Description { get; }
+
+        public DateTime? StartDate { get; }
+        public DateTime? EndDate { get; }
     }
 }
