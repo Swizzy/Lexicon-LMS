6c90b47b85741d78c7536ef45abc5df5605118dd
Swizzy <swizzy@xeupd.com>
2017-03-28 08:52:40 +0200

    Add Random Password generation to the register views

---
+++ LexiconLMS/Scripts/RandomPassword.js
@@ -0,0 +1,8 @@
+﻿function generateRandomPassword(length) {
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    var retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
